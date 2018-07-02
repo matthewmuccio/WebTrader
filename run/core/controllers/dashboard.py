508f3ca343371351bb140fc82cf500724308fe27
Matthew Muccio <me@matthewmuccio.com>
2018-07-02 16:22:58 -0400

    Adds a link with the user's username and a feather icon to the navbar adjacent to the sign out link; also refactors each of the templates that extend the base template to change two of the feather icons on the sidebar.

--- run/core/controllers/dashboard.py
+++ run/core/controllers/dashboard.py
@@ -9,49 +9,49 @@ controller = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 @controller.route("/", methods=["GET"])
 def show_dashboard():
 	if "username" in session:
-		return render_template("dashboard.html", title="Home")
+		return render_template("dashboard.html", title="Home", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/balance", methods=["GET"])
 def show_balance():
 	if "username" in session:
-		return render_template("balance.html", title="Balance")
+		return render_template("balance.html", title="Balance", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/buy", methods=["GET"])
 def show_buy():
 	if "username" in session:
-		return render_template("buy.html", title="Buy")
+		return render_template("buy.html", title="Buy", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/sell", methods=["GET"])
 def show_sell():
 	if "username" in session:
-		return render_template("sell.html", title="Sell")
+		return render_template("sell.html", title="Sell", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/lookup", methods=["GET"])
 def show_lookup():
 	if "username" in session:
-		return render_template("lookup.html", title="Lookup")
+		return render_template("lookup.html", title="Lookup", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/quote", methods=["GET"])
 def show_quote():
 	if "username" in session:
-		return render_template("quote.html", title="Quote")
+		return render_template("quote.html", title="Quote", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/portfolio", methods=["GET"])
 def show_portfolio():
 	if "username" in session:
-		return render_template("portfolio.html", title="Portfolio")
+		return render_template("portfolio.html", title="Portfolio", username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
