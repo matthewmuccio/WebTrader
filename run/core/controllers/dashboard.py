653abb914a4b6444b1c1c149e9a8b29f8d1a1b6d
Matthew Muccio <me@matthewmuccio.com>
2018-07-01 20:11:02 -0400

    Dashboard and balance templates now extend the base template and have corresponding URL routing.

--- run/core/controllers/dashboard.py
+++ run/core/controllers/dashboard.py
@@ -10,6 +10,30 @@ controller = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 def show_dashboard():
 	return render_template("dashboard.html")
 
-@controller.route("/<username>", methods=["GET", "POST"])
-def show_dash(username):
+@controller.route("/balance", methods=["GET"])
+def show_balance():
+	return render_template("balance.html")
+
+@controller.route("/buy", methods=["GET"])
+def show_buy():
+	return render_template("buy.html")
+
+@controller.route("/sell", methods=["GET"])
+def show_sell():
+	return render_template("sell.html")
+
+@controller.route("/lookup", methods=["GET"])
+def show_lookup():
+	return render_template("lookup.html")
+
+@controller.route("/quote", methods=["GET"])
+def show_quote():
+	return render_template("quote.html")
+
+@controller.route("/portfolio", methods=["GET"])
+def show_portfolio():
+	return render_template("portfolio.html")
+
+@controller.route("/<text>", methods=["GET"])
+def show_dash(text):
 	return render_template("dashboard.html")
