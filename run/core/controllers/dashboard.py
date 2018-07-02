662a214c1f451442ce63e4b62d6b447f61f9fe7c
Matthew Muccio <me@matthewmuccio.com>
2018-07-02 12:11:19 -0400

    Adds a title variable to the base template so that each template has its correct corresponding page title.

--- run/core/controllers/dashboard.py
+++ run/core/controllers/dashboard.py
@@ -9,49 +9,49 @@ controller = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 @controller.route("/", methods=["GET"])
 def show_dashboard():
 	if "username" in session:
-		return render_template("dashboard.html")
+		return render_template("dashboard.html", title="Home")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/balance", methods=["GET"])
 def show_balance():
 	if "username" in session:
-		return render_template("balance.html")
+		return render_template("balance.html", title="Balance")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/buy", methods=["GET"])
 def show_buy():
 	if "username" in session:
-		return render_template("buy.html")
+		return render_template("buy.html", title="Buy")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/sell", methods=["GET"])
 def show_sell():
 	if "username" in session:
-		return render_template("sell.html")
+		return render_template("sell.html", title="Sell")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/lookup", methods=["GET"])
 def show_lookup():
 	if "username" in session:
-		return render_template("lookup.html")
+		return render_template("lookup.html", title="Lookup")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/quote", methods=["GET"])
 def show_quote():
 	if "username" in session:
-		return render_template("quote.html")
+		return render_template("quote.html", title="Quote")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/portfolio", methods=["GET"])
 def show_portfolio():
 	if "username" in session:
-		return render_template("portfolio.html")
+		return render_template("portfolio.html", title="Portfolio")
 	else:
 		return redirect(url_for("signup.show_signup"))
 
