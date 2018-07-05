b5303cea681bddfffcec1e745b359346b30e63af
Matthew Muccio <me@matthewmuccio.com>
2018-07-05 11:34:49 -0400

    Refactors code in the dashboard controller to import the model in order to implement core functionality and database support. Also implements the front-end in the balance and buy templates, and adds a new stylesheet for the templates that extend the base template.

--- run/core/controllers/dashboard.py
+++ run/core/controllers/dashboard.py
@@ -3,55 +3,72 @@
 
 from flask import abort, Blueprint, redirect, render_template, session, url_for
 
+from core.models import model
+
 
 controller = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
 @controller.route("/", methods=["GET"])
 def show_dashboard():
 	if "username" in session:
-		return render_template("dashboard.html", title="Home", username=session["username"])
+		return render_template("dashboard.html", \
+								title="Home", \
+								username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/balance", methods=["GET"])
 def show_balance():
 	if "username" in session:
-		return render_template("balance.html", title="Balance", username=session["username"])
+		return render_template("balance.html", \
+								title="Balance", \
+								username=session["username"], \
+								balance=format(model.get_balance(session["username"]), ".2f"))
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/buy", methods=["GET"])
 def show_buy():
 	if "username" in session:
-		return render_template("buy.html", title="Buy", username=session["username"])
+		return render_template("buy.html", \
+								title="Buy", \
+								username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/sell", methods=["GET"])
 def show_sell():
 	if "username" in session:
-		return render_template("sell.html", title="Sell", username=session["username"])
+		return render_template("sell.html", \
+								title="Sell", \
+								username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/lookup", methods=["GET"])
 def show_lookup():
 	if "username" in session:
-		return render_template("lookup.html", title="Lookup", username=session["username"])
+		return render_template("lookup.html", \
+								title="Lookup", \
+								username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/quote", methods=["GET"])
 def show_quote():
 	if "username" in session:
-		return render_template("quote.html", title="Quote", username=session["username"])
+		return render_template("quote.html", \
+								title="Quote", \
+								username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
 @controller.route("/portfolio", methods=["GET"])
 def show_portfolio():
 	if "username" in session:
-		return render_template("portfolio.html", title="Portfolio", username=session["username"])
+		return render_template("portfolio.html", \
+								title="Portfolio", \
+								username=session["username"])
 	else:
 		return redirect(url_for("signup.show_signup"))
 
