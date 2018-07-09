f140b1b378938c43815e9bc1c2bc217bdd20e179
Matthew Muccio <me@matthewmuccio.com>
2018-07-08 23:35:33 -0400

    Adds correct URL routing to the newly-created admin user templates.

--- run/core/controllers/admin.py
+++ run/core/controllers/admin.py
@@ -3,7 +3,7 @@
 
 from flask import abort, Blueprint, redirect, render_template, request, session, url_for
 
-from core.models import model
+#from core.models import model
 
 
 controller = Blueprint("admin", __name__, url_prefix="/admin")
@@ -14,62 +14,62 @@ def show_dashboard():
 	if "username" in session and session["username"] == "admin":
 		return render_template("admin.html", \
                                 title="Home", \
-								username="admin")
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
 
-@controller.route("/balance", methods=["GET"])
+@controller.route("/balance", methods=["GET", "POST"])
 def show_balance():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-balance.html", \
+                                title="Balance", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
 
-@controller.route("/deposit", methods=["GET"])
+@controller.route("/deposit", methods=["GET", "POST"])
 def show_deposit():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-deposit.html", \
+                                title="Deposit", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
 
-@controller.route("/withdraw", methods=["GET"])
+@controller.route("/withdraw", methods=["GET", "POST"])
 def show_withdraw():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-withdraw.html", \
+                                title="Withdraw", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
 
-@controller.route("/set", methods=["GET"])
+@controller.route("/set", methods=["GET", "POST"])
 def show_set():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-set.html", \
+                                title="Set Balance", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
 
-@controller.route("/portfolio", methods=["GET"])
+@controller.route("/portfolio", methods=["GET", "POST"])
 def show_portfolio():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-portfolio.html", \
+                                title="Portfolio", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
@@ -78,9 +78,9 @@ def show_portfolio():
 def show_leaderboard():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-leaderboard.html", \
+                                title="Leaderboard", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
@@ -89,9 +89,9 @@ def show_leaderboard():
 def show_users():
 	# In session (user signed in) and username is admin
 	if "username" in session and session["username"] == "admin":
-		return render_template("admin.html", \
-                                title="Home", \
-								username="admin")
+		return render_template("admin-users.html", \
+                                title="Users", \
+								username="Admin")
 	# Out of session (user not signed in)
 	else:
 		return redirect(url_for("signup.show_signup"))
